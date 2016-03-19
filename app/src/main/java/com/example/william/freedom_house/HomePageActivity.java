09da4fafa200508af6dcee0b5907ae7b5fb29fab
rmwalsh <rmwalsh@norwoodlight.com>
2016-03-19 15:16:16 -0400

    Revert "WELP"
    This reverts commit 96aa4156ce26c07c114eadbe90982e2a1d74c240.

--- app/src/main/java/com/example/william/freedom_house/HomePageActivity.java
+++ app/src/main/java/com/example/william/freedom_house/HomePageActivity.java
@@ -5,7 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-port android.support.v4.app.Fragment;
+
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -16,7 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-imrt android.widget.TextView;
+import android.widget.TextView;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -53,16 +54,19 @@ public class HomePageActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-      Float  ingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-      fab.s  etOnClickListener(new View.OnClickListener() {
-          @Over  ride
-          publi  c void onClick(View view) {
-              Snack  bar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                      .setA  ction("Action", null).show();
-          }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
         });
 
-        @Override
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home_page, menu);
@@ -84,11 +88,7 @@ public class HomePageActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-
-
     /**
-     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
@@ -121,7 +121,9 @@ public class HomePageActivity extends AppCompatActivity {
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
-    }A     /**
+    }
+
+    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -155,35 +157,6 @@ public class HomePageActivity extends AppCompatActivity {
                     return "SECTION 3";
             }
             return null;
-        }
-    }placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-             public PlaceholderFragment() {
-        }Pl        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }de
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInsta                 nceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home_page, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
         }
     }
 }
